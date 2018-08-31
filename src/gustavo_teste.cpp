3bea3d01ff0b5b8909d843e5df693db9ffcd2d99
Gustavo Lahr <gjgl.lahr@gmail.com>
2018-08-31 16:19:43 +0200

    Implementação em cpp do feedback.

--- src/gustavo_teste.cpp
+++ src/gustavo_teste.cpp
@@ -1,17 +1,62 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
-#include <threading_gustavo/ApproachControllerAction.h>
-#include <threading_gustavo/ApproachControllerGoal.h>
 #include "../../../devel/include/threading_gustavo/ApproachControllerAction.h"
-#include "../../generic_control_toolbox/include/generic_control_toolbox/kdl_manager.hpp"
-#include <iostream>
-#include <geometry_msgs/Twist.h>
-
-#include <threading_gustavo/AdmittanceControllerAction.h>
-#include <threading_gustavo/AdmittanceControllerGoal.h>
 #include "../../../devel/include/threading_gustavo/AdmittanceControllerAction.h"
+//#include "../../generic_control_toolbox/include/generic_control_toolbox/kdl_manager.hpp"
+#include <iostream>
+
+//#include <geometry_msgs/Twist.h>
+//#include <threading_gustavo/AdmittanceControllerAction.h>
+//#include <threading_gustavo/ApproachControllerAction.h>
+//#include <threading_gustavo/ApproachControllerGoal.h>
 
 //using namespace generic_control_toolbox;
+
+// void done_cb(const threading_gustavo::AdmittanceControllerResult& state,
+//             const threading_gustavo::AdmittanceControllerResultConstPtr& result)
+// {
+//     ROS_INFO("soh pra nao passar em branco");
+// }
+
+double *meuValor = new double;
+
+void done_cb(const actionlib::SimpleClientGoalState& state,
+            const threading_gustavo::AdmittanceControllerResultConstPtr& result)
+{
+    ROS_INFO("soh pra nao passar em branco");
+}
+
+
+void active_cb()
+{
+  ROS_INFO("Goal just went active");
+}
+
+
+void feedback_cb(const threading_gustavo::AdmittanceControllerFeedbackConstPtr& feedback)
+{
+    
+    try {
+        int tamanho = feedback->linear_error_norm.size();
+        //std::cout << tamanho << std::endl;
+        if (tamanho == 1)
+        {
+            *meuValor = feedback->linear_error_norm[0];
+            std::cout << (*feedback).linear_error_norm[0] << std::endl;
+            //return (*feedback).linear_error_norm[0];
+        }
+        
+        if (tamanho == 2)
+            std::cout << "TAMANHO 2" << std::endl;
+            
+    //ROS_INFO("Got Feedback of length %lu", feedback->linear_error_norm.size());
+        //ROS_INFO("Got Feedback of length %f", meuvalor);
+    }
+    catch(std::runtime_error err) {
+        std::cout << "What was that?";
+        //break;
+    }
+}
 
 int main (int argc, char **argv)
 {
@@ -23,7 +68,12 @@ int main (int argc, char **argv)
   //ADMITTANCE
   actionlib::SimpleActionClient<threading_gustavo::AdmittanceControllerAction> ac("/admittance_controller/admittance", true);
   ac.waitForServer(); //will wait for infinite time
+  
   threading_gustavo::AdmittanceControllerGoal initial_pose;
+  //threading_gustavo::AdmittanceControllerActionFeedback feedback;
+  
+  //feedback.header.frame_id = "yumi_link_7_l";
+  
   initial_pose.use_left = true;
   initial_pose.desired_left_pose.header.frame_id = "yumi_link_7_l";
   initial_pose.desired_left_pose.pose.position.x = 0.4;
@@ -31,18 +81,41 @@ int main (int argc, char **argv)
   initial_pose.desired_left_pose.pose.position.z = 0.25;
   initial_pose.desired_left_pose.pose.orientation.x = 1;
   
-  std::cout << initial_pose << std::endl;
   
-  ac.sendGoal(initial_pose);
+  ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);
   
+  
+  double delta = 0;
+  
+  while(true)
+  {
+      if(delta - *meuValor != 0)
+      {
+        std::cout << *meuValor << std::endl;
+        delta = *meuValor;
+      }
+  }    
+   
+  
+  std::cout << "esperando \n" << std::endl;
   
 //   ROS_INFO("waiting");
-  ac.waitForResult(ros::Duration(30.0));
+  ac.waitForResult(ros::Duration(10.0));
 //   ROS_INFO("pass");
   
   ac.cancelGoal();
+  std::cout << "acabou a espera \n" << std::endl;
+  ac.stopTrackingGoal();
   
-  //APPROACH
+  std::cout << "sending new goal" << std::endl;
+  ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);
+  ac.waitForResult(ros::Duration(10.0));
+  ac.cancelGoal();
+  ac.stopTrackingGoal();
+  
+  
+  
+// // // // // // // // // // // // // // // // // // // // // // // //   //APPROACH
   actionlib::SimpleActionClient<threading_gustavo::ApproachControllerAction> ac_approach("/approach_controller/approach", true);
   
 //   ROS_INFO("Waiting for action server to start.");
@@ -54,7 +127,7 @@ int main (int argc, char **argv)
   
   insertion.desired_twist.twist.linear.z = 0.01;
   insertion.desired_twist.header.frame_id = "yumi_link_7_l";
-  insertion.max_contact_force = 30;
+  insertion.max_contact_force = 1.0;
   
   
   ac_approach.sendGoal(insertion);	
@@ -64,7 +137,7 @@ int main (int argc, char **argv)
   //ROS_INFO("Antes");
   //ROS_INFO(begin);
   //wait for the action to return
-  bool finished_before_timeout = ac_approach.waitForResult(ros::Duration(0.0));
+  ac_approach.waitForResult(ros::Duration(0.0));
   //ROS_INFO("Depois");
   std::cout << "[ INFO] [" << ros::Time::now() << "]: Depois" << std::endl;
   //ROS_INFO();
