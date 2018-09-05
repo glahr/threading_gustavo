aedee9c5905098d7c5fe7ac402fa6a7840ba113a
Gustavo Lahr <gjgl.lahr@gmail.com>
2018-09-05 16:32:28 +0200

    Funcionamento da etapa de controle puro de forca e inicio da programacao das minhas etapas

---
+++ include/admittance_controller.hpp
@@ -0,0 +1,107 @@
+#ifndef __ADMITTANCE_CONTROLLER__
+#define __ADMITTANCE_CONTROLLER__
+
+#include <ros/ros.h>
+#include <stdexcept>
+#include <yumi_experiments/AdmittanceControllerAction.h>
+#include <generic_control_toolbox/kdl_manager.hpp>
+#include <generic_control_toolbox/wrench_manager.hpp>
+#include <generic_control_toolbox/controller_template.hpp>
+#include <generic_control_toolbox/ArmInfo.h>
+#include <generic_control_toolbox/matrix_parser.hpp>
+
+typedef Eigen::Matrix<double, 6, 1> Vector6d;
+
+static unsigned int LEFT_ARM = 0, RIGHT_ARM = 1;
+namespace yumi_experiments
+{
+  class AdmittanceController : public generic_control_toolbox::ControllerTemplate<AdmittanceControllerAction,
+                                                                                  AdmittanceControllerGoal,
+                                                                                  AdmittanceControllerFeedback,
+                                                                                  AdmittanceControllerResult>
+  {
+  public:
+    AdmittanceController(const std::string action_name);
+    virtual ~AdmittanceController();
+
+  private:
+    sensor_msgs::JointState controlAlgorithm(const sensor_msgs::JointState &current_state, const ros::Duration &dt);
+    bool parseGoal(boost::shared_ptr<const AdmittanceControllerGoal> goal);
+    bool init();
+    void resetController();
+
+    /**
+      Saturate the commanded joint velocities if they violate the joint limits.
+      TODO: Factorize to parent class
+
+      @param eef The eef key in the KDL manager.
+      @param q The current joint positions.
+      @param desired_q_dot The desired joint velocities.
+      @param dt Current time step.
+      @retruen False in case of error, true otherwise.
+    **/
+    bool enforceJointLimits(const std::string &eef, const KDL::JntArray &q, KDL::JntArray &desired_q_dot, double dt) const;
+
+    /**
+      Computes the operational space error between the desired end-effector pose and
+      the one obtained from forward kinematics.
+
+      @param desired_pose The desired cartesian pose of the end-effector.
+      @param pose The current cartesian pose of the end-effector.
+      @param error The operational space error. The orientation error is represented with an Euler ZYZ parametrization.
+    **/
+    void computeAdmittanceError(const KDL::Frame &desired_pose, const KDL::Frame &pose, Vector6d &error) const;
+
+    /**
+      Compute the cartesian acceleration for an end-effector given current and desired poses, and measured velocity and wrench.
+    **/
+    Vector6d computeCartesianAccelerations(const Vector6d &vel_eig, const KDL::Frame &pose, const Eigen::Affine3d &desired_pose, const Vector6d &wrench) const;
+
+    /**
+      Apply a dead zone to a wrench value based on the force_dead_zone_ and
+      torque_dead_zone_ values. The controller will only take into account force
+      and torque values which have a magnitude higher than the dead zone values.
+
+      @param wrench The measured wrench
+      @return The "dead zoned" wrench.
+    **/
+    Vector6d applyDeadZone(const Vector6d &wrench) const;
+
+    /**
+      Saturate the given cartesian acceleration.
+    **/
+    Vector6d saturateAcc(const Vector6d &acc) const;
+
+    /**
+      Saturate the given cartesian velocity.
+    **/
+    Vector6d saturateVel(const Vector6d &vel) const;
+
+    /**
+      Checks if the cartesian error is bellow specified thresholds.
+
+      @param desired_pose Target pose
+      @param current_pose Measured pose
+      @return True is the error between poses is below the linear and angular thresholds, false otherwise.
+    **/
+    bool checkSuccess(const Eigen::Affine3d &desired_pose, const KDL::Frame &current_pose);
+
+    /**
+      Detects when a cartesian velocity is bigger than what could ever
+      be expected.
+    **/
+    bool checkAbsurdVelocities(const Vector6d &velocity) const;
+
+    ros::NodeHandle nh_;
+    std::shared_ptr<generic_control_toolbox::KDLManager> kdl_manager_;
+    std::vector<std::string> eef_name_;
+    std::vector<Eigen::Affine3d> desired_pose_;
+    std::vector<Vector6d> cart_vel_;
+    Eigen::Matrix<double, 6, 6> B_, K_d_;
+    Eigen::MatrixXd K_p_;
+    bool use_right_, use_left_, avoid_joint_limits_;
+    double force_dead_zone_, torque_dead_zone_, pos_offset_, max_lin_acc_, max_ang_acc_, max_lin_vel_, max_ang_vel_, linear_threshold_, angular_threshold_;
+    generic_control_toolbox::WrenchManager wrench_manager_;
+  };
+}
+#endif
--- src/gustavo_teste.cpp
+++ src/gustavo_teste.cpp
@@ -2,23 +2,16 @@
 #include <actionlib/client/simple_action_client.h>
 #include "../../../devel/include/threading_gustavo/ApproachControllerAction.h"
 #include "../../../devel/include/threading_gustavo/AdmittanceControllerAction.h"
-#include "../../generic_control_toolbox/include/generic_control_toolbox/kdl_manager.hpp"
-#include "../../yumi_experiments/include/yumi_experiments/admittance_controller.hpp"
+#include "../include/admittance_controller.hpp"
 #include <iostream>
 //#include <robot_model.h>
 #include <tf/transform_listener.h>
 
-#include <moveit/robot_model_loader/robot_model_loader.h>
-#include <moveit/robot_model/robot_model.h>
-#include <moveit/robot_state/robot_state.h>
-
-
 using namespace generic_control_toolbox;
 using namespace yumi_experiments;
 
-double *meuValor = new double;
+geometry_msgs::PoseStamped *meuValor = new geometry_msgs::PoseStamped;
 sensor_msgs::JointState states;
-// tf::TransformListener myKinematics;
 
 void done_cb(const actionlib::SimpleClientGoalState& state,
             const threading_gustavo::AdmittanceControllerResultConstPtr& result)
@@ -36,50 +29,14 @@ void active_cb()
 void feedback_cb(const threading_gustavo::AdmittanceControllerFeedbackConstPtr& feedback)
 {
     
-//         ros::spinOnce(); //esse cara ta aqui porque assim eu consigo ler as juntas no callback
-        
-//         std::shared_ptr<generic_control_toolbox::KDLManager> kdl_manager_;
-//         
-//         sensor_msgs::JointState ret = states;
-//         sensor_msgs::JointState current_state = states;
-//         std::vector<KDL::Frame> pose(2);
-//         std::vector<KDL::Twist> vel(2), desired_vel(2);
-//         std::vector<Eigen::Affine3d> pose_eig(2);
-//         std::vector<Vector6d > vel_eig(2);
-//         std::vector<Vector6d > wrench(2);
-//         std::vector<Vector6d > acc(2);
-//         std::vector<Vector6d > error(2);
-//         char eef_name[][20] = {"yumi_link_7_l", "yumi_link_7_r"};
-//         generic_control_toolbox::WrenchManager wrench_manager_;
-// 
-//         kdl_manager_->getGrippingPoint(eef_name[LEFT_ARM], current_state, pose[LEFT_ARM]);
-// //         kdl_manager_->getGrippingPoint(eef_name_[RIGHT_ARM], current_state, pose[RIGHT_ARM]);
-//         tf::transformKDLToEigen(pose[LEFT_ARM], pose_eig[LEFT_ARM]);
-// //         tf::transformKDLToEigen(pose[RIGHT_ARM], pose_eig[RIGHT_ARM]);
-//         kdl_manager_->getGrippingTwist(eef_name[LEFT_ARM], current_state, vel[LEFT_ARM]);
-// //         kdl_manager_->getGrippingTwist(eef_name_[RIGHT_ARM], current_state, vel[RIGHT_ARM]);
-//         tf::twistKDLToEigen(vel[LEFT_ARM], vel_eig[LEFT_ARM]);
-// //         tf::twistKDLToEigen(vel[RIGHT_ARM], vel_eig[RIGHT_ARM]);
-//         wrench_manager_.wrenchAtGrippingPoint(eef_name[RIGHT_ARM], wrench[RIGHT_ARM]);
-// //         wrench_manager_.wrenchAtGrippingPoint(eef_name_[LEFT_ARM], wrench[LEFT_ARM]);
-//         
-//         std::cout << pose[LEFT_ARM].p[0] << std::endl;
-        
-        
         int tamanho = feedback->linear_error_norm.size();
         //std::cout << tamanho << std::endl;
         if (tamanho == 1)
         {
-//             *meuValor = feedback->linear_error_norm[0];
-//             std::cout << states.position[0] << std::endl;
-//             std::cout << (*feedback).linear_error_norm[0] << std::endl;
-            //return (*feedback).linear_error_norm[0];
-            
-            
+//             *meuValor = feedback->actual_pose;
+//             std::cout << *meuValor << std::endl;
+//             std::cout << feedback->linear_error_norm[0] << std::endl;            
         }
-        
-        if (tamanho == 2)
-            std::cout << "TAMANHO 2" << std::endl;
             
 }
 
@@ -97,143 +54,99 @@ int main (int argc, char **argv)
 //  ros::NodeHandle tn_; //threading node
 //  ros::Subscriber states_sub = tn_.subscribe("/yumi/joint_states",1,statesCb);
   
-   
-  
-//   robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-//   robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
-//   ROS_INFO("Model frame: %s", kinematic_model->getModelFrame().c_str());
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   //MOVING TO STARTING POSITION
   
-  //ADMITTANCE
   actionlib::SimpleActionClient<threading_gustavo::AdmittanceControllerAction> ac("/admittance_controller/admittance", true);
   ac.waitForServer(); //will wait for infinite time
   
   threading_gustavo::AdmittanceControllerGoal initial_pose;
+  threading_gustavo::AdmittanceControllerGoal aux_pose;
   //threading_gustavo::AdmittanceControllerActionFeedback feedback;
   
   //feedback.header.frame_id = "yumi_link_7_l";
   
   initial_pose.use_left = true;
   initial_pose.desired_left_pose.header.frame_id = "yumi_link_7_l";
+  initial_pose.pure_ft_control = false;
 
   initial_pose.desired_left_pose.pose.position.x = 0.4;
-  initial_pose.desired_left_pose.pose.position.y = 0.0;
+  initial_pose.desired_left_pose.pose.position.y = 0.08;
   initial_pose.desired_left_pose.pose.position.z = 0.25;
   initial_pose.desired_left_pose.pose.orientation.x = 1;
   
-  
-//   const threading_gustavo::AdmittanceControllerFeedbackConstPtr& feedback = nullptr;
-//   int a;
-  
-//   ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb(feedback, a));
+  std::cout << "APPROACH \n" << std::endl;
   ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);
-//   std::cout << a << std::endl;
+  ac.waitForResult(ros::Duration(20.0));  
+  ac.cancelGoal();
+  ac.stopTrackingGoal();
   
-  
-// // // // // //   MY TRY ON FK
-  
-//   std::string arm_name = "yumi_link_7_l";
+// //   std::cout << "sending new goal" << std::endl;
+// 
+//   initial_pose.desired_left_pose.pose.position.x = 0.3;
+//   initial_pose.desired_left_pose.pose.position.y = 0.1;
+//   initial_pose.desired_left_pose.pose.position.z = 0.3;
+//   initial_pose.desired_left_pose.pose.orientation.x = 0;
 //   
-//   generic_control_toolbox::ArmInfo info;
-//   std::string base_frame;
-//   tn_.getParam("kinematic_chain_base_link", base_frame);
-//   std::shared_ptr<generic_control_toolbox::KDLManager> kdl_manager;
-//   kdl_manager = std::make_shared<generic_control_toolbox::KDLManager>(base_frame);
-//   generic_control_toolbox::setKDLManager(info, kdl_manager);
-//   KDL::Frame out;
-//   kdl_manager getEefPose("yumi_link_7_l", states, out);
-//   
-//   std::cout << info.
-//   
-  
-// // // // // // // // // //   
-  
-  
-  // rotina para pegar o valor do feedback. Não consegui pensar um jeito melhor ainda
-// //   double delta = 0;
-// //   while(true)
-// //   {
-// //       if(delta - *meuValor != 0)
-// //       {
-// //         std::cout << *meuValor << std::endl;
-// //         delta = *meuValor;
-// //       }
-// //   }
-   
-  
-//  std::cout << "esperando \n" << std::endl;
-  
-  std::cout << "TROQUEI 1 \n" << std::endl;
-  ac.waitForResult(ros::Duration(10.0));
-  
-  
-//   ac.cancelGoal();
-// //   std::cout << "acabou a espera \n" << std::endl;
-//   ac.stopTrackingGoal();
-  
-//   std::cout << "sending new goal" << std::endl;
-
-  initial_pose.desired_left_pose.pose.position.x = 0.4;
-  initial_pose.desired_left_pose.pose.position.y = 0.0;
-  initial_pose.desired_left_pose.pose.position.z = 0.3;
-  initial_pose.desired_left_pose.pose.orientation.x = 1;
-  
-  ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);  
-  std::cout << "TROQUEI 2 \n" << std::endl;
-  ac.waitForResult(ros::Duration(10.0));
+//   ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);  
+//   std::cout << "TROQUEI 2 \n" << std::endl;
+//   ac.waitForResult(ros::Duration(10.0));
 //   ac.cancelGoal();
 //   ac.stopTrackingGoal();
   
-  initial_pose.desired_left_pose.pose.position.x = 0.3;
-  initial_pose.desired_left_pose.pose.position.y = 0.1;
-  initial_pose.desired_left_pose.pose.position.z = 0.3;
-  initial_pose.desired_left_pose.pose.orientation.x = 1;
+//   aux_pose.desired_left_pose.pose.position.x = 0.4;
+//   aux_pose.desired_left_pose.pose.position.y = 0.08;
+//   aux_pose.desired_left_pose.pose.position.z = 0.25;
+//   aux_pose.pure_ft_control = true;
   
+//   aux_pose.desired_left_pose.pose.orientation.x = -0.146;
+//   aux_pose.desired_left_pose.pose.orientation.y = 0.354;
+//   aux_pose.desired_left_pose.pose.orientation.z = 0.354;
+//   aux_pose.desired_left_pose.pose.orientation.w = 0.854;
+//   aux_pose.desired_left_pose.pose.orientation.x = 1;
+  
+//   ac.sendGoal(aux_pose, &done_cb, &active_cb, &feedback_cb);  
+//   std::cout << "PONTO 2 \n" << std::endl;
+//   ac.waitForResult(ros::Duration(0.0));
+  
+  initial_pose.pure_ft_control = true;
   ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);  
-  std::cout << "TROQUEI 3 \n" << std::endl;
-  ac.waitForResult(ros::Duration(10.0));
+  std::cout << "ALIGNMENT \n" << std::endl;
+  ac.waitForResult(ros::Duration(0.0));
+  ac.cancelGoal();
+  ac.stopTrackingGoal();
   
   
-  std::cout << "ENTRANDO NO SENO \n" << std::endl;
-  ros::Duration(1.0).sleep();
+  std::cout << "THREADING \n" << std::endl;
+  initial_pose.pure_ft_control = false;
+  ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);
   
-  ros::Rate r(100);
-  double begin = ros::Time::now().toSec();
+//   std::cout << "LOOP INFINITO \n" << std::endl;
+//   while(true)
+//   {
+//       ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);
+//       ac.waitForResult(ros::Duration(0.0));
+//       ac.sendGoal(aux_pose, &done_cb, &active_cb, &feedback_cb);  
+//       ac.waitForResult(ros::Duration(0.0));
+//   }
   
-  while(ros::ok())
-  {
-      
-    
-    double t = ros::Time::now().toSec() - begin;
-    initial_pose.desired_left_pose.pose.position.x = 0.3 + 0.1*sin(0.2*t);
-    initial_pose.desired_left_pose.pose.position.y = 0.1;
-    initial_pose.desired_left_pose.pose.position.z = 0.3;
-    initial_pose.desired_left_pose.pose.orientation.x = 1;
-    
-    ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);
-    r.sleep();
-  }
+  
+//   std::cout << "ENTRANDO NO SENO \n" << std::endl;
+//   ros::Duration(1.0).sleep();
+//   
+//   ros::Rate r(100);
+//   double begin = ros::Time::now().toSec();
+//   
+//   while(ros::ok())
+//   { 
+//     double t = ros::Time::now().toSec() - begin;
+//     initial_pose.desired_left_pose.pose.position.x = 0.3 + 0.1*sin(0.2*t);
+//     initial_pose.desired_left_pose.pose.position.y = 0.1;
+//     initial_pose.desired_left_pose.pose.position.z = 0.3;
+//     initial_pose.desired_left_pose.pose.orientation.x = 1;
+//     
+//     ac.sendGoal(initial_pose, &done_cb, &active_cb, &feedback_cb);
+//     r.sleep();
+//   }
   
   
 // // // // // // // // // // // // // // // // // // // // // // // //   //APPROACH
